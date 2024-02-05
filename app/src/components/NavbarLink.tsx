28fb7fc54def93f0fba91105db919162dc907286
whyyou696 <whyyousick696@gmail.com>
2024-02-05 23:35:14 +0700

    feat:carousel-Navbar

--- app/src/components/NavbarLink.tsx
+++ app/src/components/NavbarLink.tsx
@@ -1,16 +1,15 @@
 import Link from 'next/link';
-import Image from 'next/image'; // Import Image dari Next.js untuk optimalisasi gambar
-import logo from '@/assets/logo2.png'; // Gunakan path relatif dari folder public
+import Image from 'next/image';
+import logo from '@/assets/logo2.png';
 
 export default function NavbarLink () {
   return (
-    <nav className="bg-white">
+    <nav className="bg-white h-20"> {/* Mengubah tinggi navbar menjadi 24 */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-full"> {/* Menyesuaikan tinggi agar navbar penuh */}
           <div className="flex items-center">
             <div className="flex-shrink-0 mt-8 mb-8">
-              {/* Gunakan komponen Image dari Next.js untuk mengoptimalkan gambar */}
-              <Image src={logo} alt="Logo" width={100} height={100} />
+              <Image src={logo} alt="Logo" width={120} height={120} /> {/* Mengubah ukuran logo */}
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
